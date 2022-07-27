b9c679f077da4eacb2756923d1325062fe50114a
rholdorp <ralph.holdorp@gmail.com>
2022-07-27 22:19:55 +0200

    connected database to team members

--- app/routes/teammembers.js
+++ app/routes/teammembers.js
@@ -1,9 +1,25 @@
 import { Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node"; // or cloudflare/deno
+import { useLoaderData } from "@remix-run/react";
 
-export default function TeamMembersRoute() {
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  return json(await db.TeamMember.findMany());
+};
+
+export default  function TeamMembersRoute() {
+  const data = useLoaderData();
+
   return (
     <div>
       <h1>TEAMMEMBERS SECTION</h1>
+      <ul>
+      {data.map((TeamMember) => (
+        <li key={TeamMember.id}>{TeamMember.firstName} {TeamMember.lastName}</li>
+      ))}
+    </ul>
+
       <main>
         <Outlet />
       </main>
